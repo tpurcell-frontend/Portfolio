cac5c4adf752d7409e8456470158e2bccfe995ae
Travis Purcell <travis.n.purcell@gmail.com>
2025-09-29 15:07:25 -0600

    Create the planet styles & names

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,18 +1,28 @@
+import planetNames from "../data/planet-names.js";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
 function Planet(props) {
     const galaxy = props.planetResult.galaxyOption;
     const size = props.planetResult.sizeOption;
     const surface = props.planetResult.surfaceOption;
+    var planetName = '';
+
+    function generatePlanetName() {
+        planetName = planetNames[Math.floor( Math.random() * planetNames.length )];
+
+        return planetName.charAt().toUpperCase() + planetName.slice(1);
+    };
 
     return (
         <div className="planet-container">
             {props.planetResult ? 
-                <div className="planetResults">
+                <div className="planet-results">
                     <h3>Planet Results</h3>
-                    <h4>Name: Generated Name</h4>
+                    <h4>Planet Name: {generatePlanetName()}</h4>
                     <ul>
-                        <li>{galaxy}</li>
-                        <li>{size}</li>
-                        <li>{surface}</li>
+                        <li><ChevronRightIcon /><strong>Galaxy:&nbsp;</strong> {galaxy.charAt(0).toUpperCase() + galaxy.slice(1)}</li>
+                        <li><ChevronRightIcon /><strong>Size:&nbsp;</strong> {size.charAt(0).toUpperCase() + size.slice(1)}</li>
+                        <li><ChevronRightIcon /><strong>Surface:&nbsp;</strong> {surface.charAt(0).toUpperCase() + surface.slice(1)}</li>
                     </ul>
                 </div>
             : ""}
---
+++ src/data/planet-names.js
@@ -0,0 +1,24 @@
+const planetNames = [
+  "Zerathis",
+  "Nuvai Prime",
+  "Kaelor IX",
+  "Vorthera",
+  "Thalos",
+  "Jundar-3",
+  "Ecliptara",
+  "Omnion",
+  "Kryonel",
+  "Xavora",
+  "Rhexis",
+  "Dromaeon",
+  "Aethelios",
+  "Myrren",
+  "Voltrune",
+  "Tarrux",
+  "Orriva",
+  "Lunethar",
+  "Gallaxor",
+  "Yxion"
+];
+
+export default planetNames;
