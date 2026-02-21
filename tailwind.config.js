b0d9ffed643a3d4a265d7f4efbe9985267e8de2b
Travis Purcell <travis.n.purcell@gmail.com>
2026-02-21 16:26:51 -0700

    Add default padding to Tailwind container

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ export default {
     extend: {},
     container: {
       center: true,
+      padding: {   
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+      }
     }
   },
   plugins: [],
