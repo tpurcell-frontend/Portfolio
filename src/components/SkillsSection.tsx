6603b162d4f2c36f3d7c1a7f9425933b0e1ef5f2
Travis Purcell <travis.n.purcell@gmail.com>
2026-02-21 12:53:13 -0700

    Revamp the Skills Section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -6,13 +6,12 @@ export default function SkillsSection() {
     function SkillsList({title, items}: {title: string, items: string[]}) {
 
         return(
-            <div className="technology-list col-12 col-md-2 fade-in-bottom">
+            <div className="skills-section__list">
                 <h3>{title}</h3>
                 <ul>
                     {items.map((item: string, index: number) => (
                         <li
                             key={item}
-                            className="fade-in-bottom" 
                         >
                             {item}
                         </li>
@@ -25,22 +24,14 @@ export default function SkillsSection() {
     return (
         <section className="skills-section">
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h2>Skills & Knowledge</h2>
-                    </div>
-                </div>
-                <div className="skills-list-wrapper">
-                    <div className="container">
-                        <div className="row">
-                            <SkillsList title="Programming" items={['JavaScript', 'TypeScript', 'PHP', 'React 18', 'Next.js', 'Redux', 'Node.js', 'jQuery']}/>
-                            <SkillsList title="Styling" items={['CSS', 'SASS', 'CSS Modules', 'Styled-Components', 'Bootstrap', 'Material UI', 'Tailwind', 'Foundation']}/>
-                            <SkillsList title="Standards, Build Tools & AI" items={['Webpack', 'Vite', 'npm', 'Yarn', 'WCAG 2.2', 'Claude', 'CoPilot']}/>
-                            <SkillsList title="Collaboration Tools" items={['Jira', 'Confluence', 'GitHub', 'Git', 'Figma']}/>
-                            <SkillsList title="Testing" items={['Chrome Developer Tools', 'Jest', 'Cypress', 'BrowserStack']}/>
-                            <SkillsList title="CMS" items={['WordPress', 'Drupal 11']}/>
-                        </div>
-                    </div>
+                <h2>Skills & Knowledge</h2>
+                <div className="skills-section__wrapper">
+                    <SkillsList title="Programming" items={['JavaScript', 'TypeScript', 'PHP', 'React 18', 'Next.js', 'Redux', 'Node.js', 'jQuery']}/>
+                    <SkillsList title="Styling" items={['CSS', 'SASS', 'CSS Modules', 'Styled-Components', 'Bootstrap', 'Material UI', 'Tailwind', 'Foundation']}/>
+                    <SkillsList title="Standards, Build Tools & AI" items={['Webpack', 'Vite', 'npm', 'Yarn', 'WCAG 2.2', 'Claude', 'CoPilot']}/>
+                    <SkillsList title="Collaboration Tools" items={['Jira', 'Confluence', 'GitHub', 'Git', 'Figma']}/>
+                    <SkillsList title="Testing" items={['Chrome Developer Tools', 'Jest', 'Cypress', 'BrowserStack']}/>
+                    <SkillsList title="CMS" items={['WordPress', 'Drupal 11']}/>
                 </div>
             </div>
         </section>
